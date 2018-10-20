4a7663987dbaa6368c131f7fe8297e7100f4f5d3
Cr0manty <aappland@gmail.com>
2018-10-20 07:38:32 +0300

    add insert

--- new_map/new_map.h
+++ new_map/new_map.h
@@ -1,22 +1,24 @@
 #pragma once
 #include <string>
 #include <algorithm>
+#include <iostream>
 template<typename Key, typename Value>
 class new_map
 {
 	struct Element
 	{
 		Key m_key;
-		Value *m_value;
+		Value m_value;
 		enum { NOT_OCCUPIED, OCCUPIED, DELETED } m_status;
 	};
 
 	size_t m_size;
-	size_t m_end;
+	size_t m_dataSize;
 	Element* m_pData;
 
-	bool tryInsert(int _bucketNr, Key _key, Value _value);
+	bool tryInsert(size_t _bucketNr, Key _key, Value _value);
 	void resize();
+	Key HashCode(Key);
 	size_t m_begin;
 public:
 	new_map(size_t = 100);
@@ -27,20 +29,20 @@ public:
 	void clear();
 	void insert(Key, Value);
 	size_t find(Key) const;
+	void print();
 	~new_map();
 };
 
 template<typename Key, typename Value>
-inline bool new_map<Key, Value>::tryInsert(int _bucketNr, Key _key, Value _value)
+inline bool new_map<Key, Value>::tryInsert(size_t _bucketNr, Key _key, Value _value)
 {
-	//переделать под key и value
-	if (m_pData[_bucketNr].m_status != HashTable::Element::OCCUPIED)
+	if (m_pData[_bucketNr].m_status != Element::OCCUPIED)
 	{
-		m_pData[_bucketNr].m_status = HashTable::Element::OCCUPIED;
+		m_pData[_bucketNr].m_status = Element::OCCUPIED;
 		m_pData[_bucketNr].m_key = _key;
 		m_pData[_bucketNr].m_value = _value;
 
-		m_end++;
+		m_dataSize++;
 		return true;
 	}
 	else
@@ -58,17 +60,17 @@ inline void new_map<Key, Value>::resize()
 	for (int i = 0; i < m_size; i++)
 		m_pData[i].m_status = Element::NOT_OCCUPIED;
 
-	m_end = 0;
+	m_dataSize = 0;
 
 	for (int i = 0; i < oldSize; i++)
-		if (oldData[i].m_status == HashTable::Element::OCCUPIED) {
-			if ((m_end << 1) >= m_size)
+		if (oldData[i].m_status == Element::OCCUPIED) {
+			if ((m_dataSize << 1) >= m_size)
 				resize();
 
 			Key hashCode = oldData[i].m_key;
 			auto bucketNr = hashCode % m_size;
 
-			for (int i = bucketNr; i < m_size; i++)
+			for (size_t i = bucketNr; i < m_size; i++)
 				if (tryInsert(i, oldData[i].m_key, oldData[i].m_value))
 					return;
 
@@ -79,15 +81,18 @@ inline void new_map<Key, Value>::resize()
 			insert(oldData[i].m_key, oldData[i].m_value);
 		}
 
-	for (int i = 0; i < oldSize; i++)
-		if (oldData[i].m_status == HashTable::Element::OCCUPIED)
-			delete[] oldData[i].m_value;
 	delete[] oldData;
 }
 
 template<typename Key, typename Value>
+inline Key new_map<Key, Value>::HashCode(Key _key)
+{
+	return _key;
+}
+
+template<typename Key, typename Value>
 inline new_map<Key, Value>::new_map(size_t _size) :
-	m_size(_size), m_begin(0), m_end(0)
+	m_size(_size), m_begin(0), m_dataSize(0)
 {
 	m_pData = new Element[m_size];
 
@@ -103,39 +108,46 @@ inline size_t new_map<Key, Value>::begin() const
 template<typename Key, typename Value>
 inline size_t new_map<Key, Value>::end() const
 {
-	return m_end;
+	return m_dataSize;
 }
 
 template<typename Key, typename Value>
 inline size_t new_map<Key, Value>::size() const
 {
-	return m_end;
+	return m_dataSize;
 }
 
 template<typename Key, typename Value>
 inline void new_map<Key, Value>::clear()
 {
-	m_end = 0;
-	for (int i = 0; i < m_size; i++)\
-	{
+	m_dataSize = 0;
+	for (int i = 0; i < m_size; i++)
 		if (m_pData[i].m_status == Element::OCCUPIED)
-		{
-			if (m_pData[i].m_value)
-				delete[] m_pData[i].m_value;
 			m_pData[i].m_status = Element::NOT_OCCUPIED;
-		}
-	}
 }
 
 template<typename Key, typename Value>
 inline void new_map<Key, Value>::insert(Key _key, Value _value)
 {
+	if ((m_dataSize << 1) >= m_size)
+		resize();
+
+	Key hashCode = HashCode(_key);
+	size_t bucketNr = hashCode % m_size;
+
+	for (size_t i = bucketNr; i < m_size; i++)
+		if (tryInsert(i, HashCode(_key), _value))
+			return;
+
+	for (int i = 0; i < bucketNr; i++)
+		if (tryInsert(i, HashCode(_key), _value))
+			return;
 }
 
 template<typename Key, typename Value>
 inline size_t new_map<Key, Value>::find(Key _key) const
 {
-	for (size_t i = 0; i < m_end; i++)
+	for (size_t i = 0; i < m_dataSize; i++)
 		if (m_pData[i].m_status == Element::OCCUPIED)
 			if (m_pData[i].m_key == _key)
 				return i;
@@ -144,11 +156,14 @@ inline size_t new_map<Key, Value>::find(Key _key) const
 }
 
 template<typename Key, typename Value>
+inline void new_map<Key, Value>::print()
+{
+	for (size_t i = 0; i < m_dataSize; i++)
+		std::cout << m_pData[i].m_key << " " << m_pData[i].m_value << std::endl;
+}
+
+template<typename Key, typename Value>
 inline new_map<Key, Value>::~new_map()
 {
-	for (int i = 0; i < m_size; i++)
-		if (m_pData[i].m_status == Element::OCCUPIED)
-			delete[] m_pData[i].m_value;
-
 	delete[] m_pData;
 }
