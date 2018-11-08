7f16dcca4d4073210ed94e5b2b9367942b0a474f
Cr0manty <aappland@gmail.com>
2018-11-08 04:11:49 +0200

    rewrite code, broke end() and operator=. Need to know what is emplace and what is insert

--- new_map/Source.cpp
+++ new_map/Source.cpp
@@ -9,10 +9,10 @@ int main()
 
 	new_map<int, std::string> mapa;
 	std::string si = "sis";
-	for (int i = 0; i < 4; i++)
+	for (int i = 5; i < 10; i++)
 		mapa.insert(i, "sss");
 
-	new_map<int, std::string>::iterator iter = mapa.begin();
+	/*new_map<int, std::string>::iterator iter = mapa.begin();
 	new_map<int, std::string>::iterator it = mapa.end();
 
 	(*iter).second = "kik";
@@ -20,13 +20,18 @@ int main()
 	for (; iter != it; ++iter)
 		std::cout << (*iter).first << " ";
 
-	mapa.emplace(1, "sdsd");
-	std::cout << std::endl;
-	std::cout << mapa.find(0)->second << std::endl;
-	mapa.erase(iter);
+	mapa.emplace(1, "sdsd");*/
 
-	for (auto i : mapa)
-		std::cout << i.first << " " << i.second << std::endl;
+	/*std::cout << mapa[1].first << std::endl;
+	std::cout << mapa.find(0)->second << std::endl;*/
+	mapa.insert(0, "sds");
+	if (mapa.find(1) == mapa.npos)
+		std::cout << "npos" << std::endl;
+
+	new_map<int, std::string>::iterator it = mapa.begin();
+	std::cout << (*it).first;
+	//for (auto i : mapa)
+	//	std::cout << i.first << " " << i.second << std::endl;
 
 	getchar();
 	return 0;
--- new_map/new_map.h
+++ new_map/new_map.h
@@ -8,30 +8,30 @@ public: class iterator;
 private:
 	class Element;
 	size_t m_size;
-	size_t m_nOccupied;
 	Element* m_pData;
-	bool _tryinsert(const size_t &, const Key &, const Value &);
-	void resize();	
+	void _destroy(Element*);
+	void _transplant(Element *, Element*); 
+	Element* _minimumNode();
+	Element* _maximumNode();
+
 public:
 	Element * const npos;
 	new_map();
-	new_map(const size_t &);
-	new_map(const new_map&);
+	new_map(const new_map&);//
 	new_map(new_map&&);
 
-	new_map &operator=(const new_map&);
+	new_map &operator=(const new_map&);//
 	new_map &operator=(new_map&&);
 	Element &operator[](const Key &);
 
 	size_t size() const;
 	bool empty() const;
-	void erase();
-	void clear();
-	void emplace(const Key &, const Value &);
+	void erase(const Key &);
+	void emplace(const Key &, const Value &);//
 	void insert(const Key &, const Value &);
 	Element* find(const Key &) const;
-	const iterator begin() const;
-	const iterator end() const;
+	iterator begin() const;
+	iterator end() const;//
 	~new_map();
 };
 
@@ -61,12 +61,13 @@ template<class BasicKey, class BasicValue>
 class new_map<BasicKey, BasicValue>::Element
 {
 	friend class new_map<BasicKey, BasicValue>;
-	enum STATUS{ NOT_OCCUPIED, OCCUPIED, DELETED, NULLPTR } m_status;
+	Element(const BasicKey &, const BasicValue &);
+	Element * m_parent;
+	Element * m_left;
+	Element * m_right;
 	bool operator==(const Element &);
 	bool const operator!=(const Element &) const;
 	operator bool();
-	Element(STATUS);
-	Element();
 public:
 	BasicKey first;
 	BasicValue second;
@@ -75,99 +76,81 @@ public:
 #endif
 
 template<class Key, class Value>
-inline bool new_map<Key, Value>::_tryinsert(const size_t &_index, const Key &_key, const Value &_value)
+inline void new_map<Key, Value>::_destroy(Element *_root)
 {
-	if (m_pData[_index].m_status != Element::OCCUPIED)
-	{
-		m_pData[_index].m_status = Element::OCCUPIED;
-		m_pData[_index].first = _key;
-		m_pData[_index].second = _value;
-		m_nOccupied++;
-		return true;
-	}
-	else
-		return false;
+	if (!_root)
+		return;
+
+	_destroy(_root->m_left);
+	_destroy(_root->m_right);
+
+	delete _root;
 }
 
 template<class Key, class Value>
-inline void new_map<Key, Value>::resize()
+inline void new_map<Key, Value>::_transplant(Element *_node, Element *_otherNode)
 {
-	size_t oldSize = m_size;
-	m_size <<= 1;
-	const Element* oldData = m_pData;
-	m_pData = new Element[m_size];
-	for (int i = 0; i < m_size; i++)
-		m_pData[i].m_status = Element::NOT_OCCUPIED;
+	if (!_node->m_parent)
+	{
+		//assert(_node == _tree.m_pRoot);
+		m_pData = _otherNode;
+	}
 
-	m_nOccupied = 0;
+	else if (_node->m_parent->m_left == _node)
+		_node->m_parent->m_left = _otherNode;
 
-	for (size_t i = 0; i < oldSize; i++)
-		if (oldData[i].m_status == Element::OCCUPIED) {
-			if ((m_nOccupied << 1) >= m_size)
-				resize();
+	else if (_node->m_parent->m_right == _node)
+		_node->m_parent->m_right = _otherNode;
 
-			for (size_t j = i; j < m_size; j++)
-				if (_tryinsert(j, oldData[j].first, oldData[j].second))
-					break;
+	//else
+	//	assert(0);
 
-			for (size_t j = 0; j < i; j++)
-				if (_tryinsert(j, oldData[j].first, oldData[j].second))
-					break;
-		}
+	if (_otherNode)
+		_otherNode->m_parent = _node->m_parent;
+}
 
-	delete[] oldData;
+template<class Key, class Value>
+inline typename new_map<Key, Value>::Element* new_map<Key, Value>::_minimumNode()
+{
+	Element * _node = _minimumNode();
+	return _node;
+}
+
+template<class Key, class Value>
+inline typename new_map<Key, Value>::Element* new_map<Key, Value>::_maximumNode()
+{
+	Element * _node = m_pData;
+	while (_node && _node->m_right)
+		_node = _node->m_right;
+
+	return _node;
 }
 
 template<class Key, class Value>
 inline new_map<Key, Value>::new_map() :
-	m_size(100), npos(new Element(Element::NULLPTR))
+	m_pData(nullptr), npos(nullptr)
 {
-	m_pData = new Element[m_size];
-
-	clear();
-}
-
-template<class Key, class Value>
-inline new_map<Key, Value>::new_map(const size_t &_size) :
-	m_size(_size), npos(new Element(Element::NULLPTR))
-{
-	m_pData = new Element[m_size];
-
-	clear();
 }
 
 template<class Key, class Value>
 inline new_map<Key, Value>::new_map(const new_map &_other)
 {
 	m_size = _other.m_size;
-	m_pData = new Element[m_size];
-	m_nOccupied = _other.m_nOccupied;
-
-	for (size_t i = 0; i < m_nOccupied; i++)
-		_tryinsert(i, _other.m_pData[i].first, _other.m_pData[i].second);
-
+	m_pData = _other.m_pData;
 	return *this;
 }
 
 template<class Key, class Value>
 inline new_map<Key, Value>::new_map(new_map &&_other):
-	m_pData(_other.m_pData), m_size(_other.m_size),
-	m_nOccupied(_other.m_nOccupied)
+	m_pData(_other.m_pData), m_size(_other.m_size)
 {
-	delete[]_other.m_pData;
-	_other.m_size = 0;
-	m_nOccupied.m_nUsed = 0;
 }
 
 template<class Key, class Value>
 inline new_map<Key, Value> & new_map<Key, Value>::operator=(const new_map &_other)
 {
 	m_size = _other.m_size;
-	m_pData = new Element[m_size];
-	m_nOccupied = _other.m_nOccupied;
-
-	for (size_t i = 0; i < m_nOccupied; i++)
-		_tryinsert(i, _other.m_pData[i].first, _other.m_pData[i].second);
+	m_pData = _other.m_pData;
 
 	return *this;
 }
@@ -177,11 +160,6 @@ inline new_map<Key, Value> & new_map<Key, Value>::operator=(new_map &&_other)
 {
 	m_pData = _other.m_pData;
 	m_size = _other.m_size;
-	m_nOccupied = _other.m_nOccupied;
-
-	delete[] _other.m_pData;
-	_other.m_nOccupied = 0;
-	_other.m_size = 0;
 
 	return *this;
 }
@@ -189,36 +167,50 @@ inline new_map<Key, Value> & new_map<Key, Value>::operator=(new_map &&_other)
 template<class Key, class Value>
 inline typename new_map<Key, Value>::Element & new_map<Key, Value>::operator[](const Key &_key)
 {
-	return find(_key);
+	return (*find(_key));
 }
 
 template<class Key, class Value>
 inline size_t new_map<Key, Value>::size() const
 {
-	return m_nOccupied;
+	return m_size;
 }
 
 template<class Key, class Value>
 inline bool new_map<Key, Value>::empty() const
 {
-	return m_nOccupied;
+	return m_size;
 }
 
 template<class Key, class Value>
-inline void new_map<Key, Value>::erase()
+inline void new_map<Key, Value>::erase(const Key &_key)
 {
-	m_nOccupied = 0;
-	delete[] m_pData;
-	m_pData = new Element[m_size];
-}
+	Element * _node = find(_key);
+	if (!_node)
+		return;
 
-template<class Key, class Value>
-inline void new_map<Key, Value >::clear()
-{
-	m_nOccupied = 0;
-	for (int i = 0; i < m_size; i++)
-		if (m_pData[i].m_status != Element::OCCUPIED)
-			m_pData[i].m_status = Element::NOT_OCCUPIED;
+	if (!_node->m_left)
+		_transplant(_node, _node->m_right);
+
+	else if (!_node->m_right)
+		_transplant(_node, _node->m_left);
+
+	else
+	{
+		Element *_newxtNode = _minimumNode();
+		if (_newxtNode->m_parent != _node)
+		{
+			_transplant(_newxtNode, _newxtNode->m_right);
+			_newxtNode->m_right = _node->m_right;
+			_newxtNode->m_right->m_parent = _newxtNode;
+		}
+
+		_transplant(_node, _newxtNode);
+		_newxtNode->m_left = _node->m_left;
+		_newxtNode->m_left->m_parent = _newxtNode;
+	}
+
+	delete _node;
 }
 
 template<class Key, class Value>
@@ -226,72 +218,98 @@ inline void new_map<Key, Value>::emplace(const Key &_key, const Value &_value)
 {
 	auto iter = find(_key);
 
-	if (iter != npos) {
+	/*if (iter != npos) {
 		iter->first = _key;
 		iter->second = _value;
 	}
-	else
+	else*/
 		insert(_key, _value);
 }
 
 template<class Key, class Value>
 inline void new_map<Key, Value>::insert(const Key &_key, const Value &_value)
 {
-	if ((m_nOccupied << 1) >= m_size)
-		this->resize();
+	m_size++;
 
-	for (size_t i = m_nOccupied; i < m_size; i++)
-		if (_tryinsert(i, _key, _value))
+	Element * _current = m_pData;
+	
+	if (!_current)
+	{
+		m_pData = new Element(_key, _value);
+		return;
+	}
+
+	while (_current)
+	{
+		if (_current->first == _key)
 			return;
 
-	for (size_t i = 1; i < m_nOccupied; i++)
-		if (_tryinsert(i, _key, _value))
-			return;
+		else if (_key < _current->first)
+		{
+			if (_current->m_left)
+				_current = _current->m_left;
+
+			else
+			{
+				Element * _newNode = new Element(_key, _value);
+				_newNode->m_parent = _current;
+				_current->m_left = _newNode;
+				return;
+			}
+		}
+
+		else
+		{
+			if (_current->m_right)
+				_current = _current->m_right;
+
+			else
+			{
+				Element* pNewNode = new Element(_key, _value);
+				pNewNode->m_parent = _current;
+				_current->m_right = pNewNode;
+				return;
+			}
+		}
+	}
 }
 
 template<class Key, class Value>
 inline typename new_map<Key, Value>::Element* new_map<Key, Value>::find(const Key &_key) const
 {
+	Element * _current = m_pData;
 
-	for (int i = m_nOccupied; i < m_size; i++)
-		if (m_pData[i].m_status == Element::NOT_OCCUPIED)
-			break;
-		else if (m_pData[i].m_status == Element::OCCUPIED &&
-			m_pData[i].first == _key)
-			return &m_pData[i];
+	while (_current)
+	{
+		if (_key == _current->first)
+			return _current;
 
-	for (int i = 0; i < m_nOccupied; i++)
-		if (m_pData[i].m_status == Element::NOT_OCCUPIED)
-			break;
-		else if (m_pData[i].m_status == Element::OCCUPIED &&
-				m_pData[i].first == _key)
-			return &m_pData[i];
+		else if (_key < _current->first)
+			_current = _current->m_left;
+
+		else
+			_current = _current->m_right;
+	}
 
 	return npos;
 }
 
 template<class Key, class Value>
-inline typename const new_map<Key, Value>::iterator new_map<Key, Value>::begin() const
+inline typename new_map<Key, Value>::iterator new_map<Key, Value>::begin() const
 {
-	size_t m_begin = 0;
-
-	while (m_pData[m_begin].m_status != Element::OCCUPIED &&
-		m_nOccupied >= m_begin) m_begin++;
-
-	return &m_pData[m_begin];
+	return m_pData;
 }
 
 template<class Key, class Value>
-inline typename const new_map<Key, Value>::iterator new_map<Key, Value>::end() const
+inline typename new_map<Key, Value>::iterator new_map<Key, Value>::end() const
 {
-	return &m_pData[m_nOccupied];
+	return _maximumNode();
 }
 
 template<class Key, class Value>
 inline new_map<Key, Value>::~new_map()
 {
-	delete npos;
-	delete[] m_pData;
+	_destroy(m_pData);
 }
 
 template<class BasicKey, class BasicValue>
@@ -389,14 +407,9 @@ inline bool new_map<BasicKey, BasicValue>::iterator::operator==(const iterator &
 }
 
 template<class BasicKey, class BasicValue>
-inline new_map<BasicKey, BasicValue>::Element::Element()
+inline new_map<BasicKey, BasicValue>::Element::Element(const BasicKey & _key, const BasicValue & _value) :
+	first(_key), second(_value), m_parent(nullptr), m_left(nullptr), m_right(nullptr)
 {
-}
-
-template<class BasicKey, class BasicValue>
-inline new_map<BasicKey, BasicValue>::Element::Element(STATUS _status)
-{
-	m_status = _status;
 }
 
 template<class BasicKey, class BasicValue>
